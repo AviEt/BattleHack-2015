27eb0813c7f50294fb38bf6e37570580f3b83774
aviet <avi.etzioni@gmail.com>
2015-10-07 18:29:43 +0300

    Added start/destination cities to routes tab

--- www/js/services.js
+++ www/js/services.js
@@ -54,27 +54,32 @@ angular.module('starter.services', [])
     var routes = [{
       id: 0,
       name: 'Ben Sparrow',
-      lastText: 'You on your way?',
+      startText: 'Tel-Aviv',
+      destinationText: 'Nethanya',
       face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
     }, {
       id: 1,
       name: 'Max Lynx',
-      lastText: 'Hey, it\'s me',
+      startText: 'Tel-Aviv',
+      destinationText: 'Nethanya',
       face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
     }, {
       id: 2,
       name: 'Adam Bradleyson',
-      lastText: 'I should buy a boat',
+      startText: 'Tel-Aviv',
+      destinationText: 'Nethanya',
       face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
     }, {
       id: 3,
       name: 'Perry Governor',
-      lastText: 'Look at my mukluks!',
+      startText: 'Tel-Aviv',
+      destinationText: 'Nethanya',
       face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
     }, {
       id: 4,
       name: 'Mike Harrington',
-      lastText: 'This is wicked good ice cream.',
+      startText: 'Tel-Aviv',
+      destinationText: 'Nethanya',
       face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
     }];
 
